601b5cb1f66f37e5052a09fa7db7042aec5c4043
liangjianfeng <769970587@qq.com>
2019-01-23 12:32:26 +0800

    add webpack dev server

--- src/app.vue
+++ src/app.vue
@@ -8,7 +8,7 @@
 export default {
     data() {
         return {
-            text: 'abc'
+            text: 'abc999999'
         }
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const webpack  = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development'
 
-module.exports = {
+const config = {
+    target: 'web',
     entry:  path.join(__dirname, 'src/index.js'),
     output: {
         filename: 'bundle.js',
@@ -41,6 +45,31 @@ module.exports = {
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new VueLoaderPlugin(),
+        new HTMLPlugin()
     ]
 }
+
+if( isDev ) {
+    config.devtool = '#cheap-module-eval-source-map',
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true
+        },
+        hot: true
+    },
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+
+}
+
+module.exports= config
