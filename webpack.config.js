445a7d35b692c38059ce560644999a79894ff051
liangjianfeng <769970587@qq.com>
2019-01-24 15:20:41 +0800

    提交打包后的文档

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const config = {
     entry:  path.join(__dirname, 'src/index.js'),
     output: {
         filename: 'bundle.[hash:8].js',
-        path: path.join(__dirname, 'dist')
+        path: path.join(__dirname, 'doc')
     },
     module: {
         rules: [
